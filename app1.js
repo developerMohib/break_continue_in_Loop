3de81c1844c10c3c63839987c0a7e5c5a8de3819
Mohib <mohibullahmohim2020@gmail.com>
2024-02-02 15:37:00 +0600

    First commit for addition

---
+++ app1.js
@@ -0,0 +1,22 @@
+
+/* ------------- Break -------------------*/
+/* 
+
+    Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+*/
+/* 
+    Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+*/
+/* 
+    Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
+*/
+/* ------------- Continue -------------------*/
+
+/* 
+    Write a loop to print even numbers from 1 to 40. Use continue to skip odd numbers.
+*/
+
+/*
+    display odd number from 55 to 85 and skip the numbers divisible by 5.
+*/
